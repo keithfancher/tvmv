ff497944ae19b8c069b154459154ce8c01d5cb64
ktf <not.discostoo@gmail.com>
2022-10-20 21:13:21 -0700

    Map errors *and* successes away from TMDB-specific types

--- src/API.hs
+++ src/API.hs
@@ -1,4 +1,9 @@
-module API (searchShowByName) where
+module API
+  ( APIKey,
+    Error,
+    searchShowByName,
+  )
+where
 
 import qualified Data.Text as T
 import qualified Network.API.TheMovieDB as TMDB
@@ -6,14 +11,13 @@ import Show (TvShow (..))
 
 type APIKey = T.Text
 
+type Error = T.Text
+
 -- Given a show name, or a fragment of a name, get back a list of matches.
--- TODO: Map error type to an internal type as well, probably.
-searchShowByName :: APIKey -> T.Text -> IO (Either TMDB.Error [TvShow])
+searchShowByName :: APIKey -> T.Text -> IO (Either Error [TvShow])
 searchShowByName key query = do
   tmdbResults <- TMDB.runTheMovieDB (TMDB.defaultSettings key) (TMDB.searchTV query)
-  return (mapShows <$> tmdbResults)
-  where
-    mapShows = map mapTvShow
+  return (mapEither tmdbResults)
 
 -- Map from TMDB format to our internal domain. (Almost identical really, but
 -- ours is a subset. Plus, mapping is always a sanity-saver.)
@@ -27,3 +31,10 @@ mapTvShow s =
       numberOfSeasons = TMDB.tvNumberOfSeasons s,
       numberOfEpisodes = TMDB.tvNumberOfEpisodes s
     }
+
+-- Remove traces of the TMDB types for both success and error responses.
+-- TODO: Map error type to an internal type as well, probably. For now, just
+-- use Text. All we're doing from here is printing it anyway.
+mapEither :: Either TMDB.Error [TMDB.TV] -> Either Error [TvShow]
+mapEither (Right s) = Right (map mapTvShow s)
+mapEither (Left e) = Left $ T.pack $ show e
