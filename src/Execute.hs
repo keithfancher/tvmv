f90685425637d2fedf9bf14fbd8edcd24cd2bd70
ktf <not.discostoo@gmail.com>
2022-11-06 20:19:13 -0800

    Implement basic `search` functionality

--- src/Execute.hs
+++ src/Execute.hs
@@ -4,20 +4,20 @@ module Execute
   )
 where
 
-import API (searchSeason)
-import Command (Command (..), MvOptions (..), SearchKey (..), UndoOptions (..))
+import API (searchSeason, searchShowByName)
+import Command (Command (..), MvOptions (..), SearchKey (..), SearchOptions (..), UndoOptions (..))
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Class (lift)
 import Error (Error)
 import File (listDir)
 import Log (readLogFile, writeLogFileAndPrint)
 import Rename (executeRename, renameFiles, undoRenameOp)
-import Show (Season (..))
+import Show (Season (..), printShows)
 import Tvmv (Tvmv, liftEither, mkTvmv, runTvmv)
 
 execCommand :: Command -> Tvmv ()
 execCommand (Mv mvOpts) = renameSeason mvOpts
-execCommand (Search _) = error "Implement me ;)" -- TODO: Other commands!
+execCommand (Search searchOpts) = searchByName searchOpts
 execCommand (Undo undoOpts) = undoRename undoOpts
 
 -- Run it with the configured logger
@@ -40,3 +40,8 @@ undoRename (UndoOptions logFileName) = do
   renameOps <- mkTvmv $ readLogFile logFileName
   let reversedOps = map undoRenameOp renameOps
   lift $ executeRename reversedOps -- TODO: do I want the undo op to also generate a log?
+
+searchByName :: SearchOptions -> Tvmv ()
+searchByName (SearchOptions key searchQuery) = do
+  showResults <- searchShowByName key searchQuery
+  liftIO $ printShows showResults -- TODO: search shouldn't gen a log either...
