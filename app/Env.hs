1c4c829dcd5f2559dbc64f457f7c2f56f69e9a67
ktf <not.discostoo@gmail.com>
2022-11-09 21:18:39 -0800

    Allow reading API key from a config file as well

--- app/Env.hs
+++ app/Env.hs
@@ -1,13 +1,44 @@
 module Env (populateEnv) where
 
+import API (APIKey)
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 import Execute (Env (..))
+import System.Directory (XdgDirectory (..), doesFileExist, getXdgDirectory)
 import System.Environment (lookupEnv)
+import System.FilePath ((</>))
 
 apiKeyEnvVarName :: String
 apiKeyEnvVarName = "TMDB_API_KEY"
 
+-- This will be relative to the config path, defined below
+apiKeyFileName :: FilePath
+apiKeyFileName = "tmdb-api-key"
+
 populateEnv :: IO Env
 populateEnv = do
-  envVar <- lookupEnv apiKeyEnvVarName
-  return $ Env {apiKeyEnvVar = T.pack <$> envVar, apiKeyFile = Nothing} -- TODO: file!
+  envVarContents <- lookupEnv apiKeyEnvVarName
+  apiKeyFromFile <- apiKeyFileContents
+  return $
+    Env
+      { apiKeyEnvVar = T.pack <$> envVarContents,
+        apiKeyFile = apiKeyFromFile
+      }
+
+apiKeyFileContents :: IO (Maybe APIKey)
+apiKeyFileContents = do
+  configDir <- configDirPath
+  let apiFileName = configDir </> apiKeyFileName
+  fileExists <- doesFileExist apiFileName
+  if fileExists
+    then Just <$> getFileContents apiFileName
+    else return Nothing
+  where
+    -- Note call to `strip` -- more than likely, the file will end with a newline:
+    getFileContents fn = T.strip <$> TIO.readFile fn
+
+-- On unix-ey systems this would be, e.g.: `~/.config/tvmv`
+configDirPath :: IO FilePath
+configDirPath = getXdgDirectory XdgConfig configDirName
+  where
+    configDirName = "tvmv"
