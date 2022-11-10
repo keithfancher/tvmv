158cdd8ea8f2df49797c42a3d3656bd3a96b2222
ktf <not.discostoo@gmail.com>
2022-11-09 18:09:07 -0800

    Initial wiring for passing in an `Env` along with command options

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,13 @@
 module Main (main) where
 
 import Args (cliOptParser)
-import Execute (execCommand, run)
+import Execute (Env (..), execCommand, run)
 import Options.Applicative (execParser)
 
 main :: IO ()
 main = do
   command <- execParser cliOptParser
-  result <- run $ execCommand command
+  result <- run $ execCommand defaultEnv command
   print result
+  where
+    defaultEnv = Env Nothing Nothing -- TODO: populate Env!
--- src/Execute.hs
+++ src/Execute.hs
@@ -1,33 +1,42 @@
 module Execute
-  ( execCommand,
+  ( Env (..),
+    execCommand,
     run,
   )
 where
 
 import API (APIKey, searchSeasonById, searchSeasonByName, searchShowByName)
 import Command (Command (..), MvOptions (..), SearchKey (..), SearchOptions (..), UndoOptions (..))
+import Control.Applicative ((<|>))
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Class (lift)
-import Error (Error (APIError))
+import Error (Error (..))
 import File (listFiles)
 import Log (readLogFile, writeLogFileAndPrint)
 import Rename (executeRename, renameFiles, undoRenameOp)
 import Show (Season (..), printShows)
 import Tvmv (Tvmv, liftEither, mkTvmv, runTvmv)
 
-execCommand :: Command -> Tvmv ()
-execCommand (Mv mvOpts) = renameSeason mvOpts
-execCommand (Search searchOpts) = searchByName searchOpts
-execCommand (Undo undoOpts) = undoRename undoOpts
+-- Wrapper for other envinroment-related stuff we might need to execute a
+-- command, in addition to the parsed-out CLI args.
+data Env = Env
+  { apiKeyEnvVar :: Maybe APIKey,
+    apiKeyFile :: Maybe APIKey
+  }
+
+execCommand :: Env -> Command -> Tvmv ()
+execCommand e (Mv mvOpts) = renameSeason e mvOpts
+execCommand e (Search searchOpts) = searchByName e searchOpts
+execCommand _ (Undo undoOpts) = undoRename undoOpts
 
 -- Run it with the configured logger
 run :: Tvmv a -> IO (Either Error a)
 run tvmv = runTvmv tvmv writeLogFileAndPrint
 
 -- Tie the pieces together, essentially.
-renameSeason :: MvOptions -> Tvmv ()
-renameSeason (MvOptions maybeApiKey searchQuery seasNum inFiles) = do
-  apiKey <- liftEither $ populateAPIKey maybeApiKey
+renameSeason :: Env -> MvOptions -> Tvmv ()
+renameSeason env (MvOptions maybeApiKey searchQuery seasNum inFiles) = do
+  apiKey <- liftEither $ populateAPIKey maybeApiKey env
   season <- searchSeason apiKey seasNum
   files <- liftIO $ listFiles inFiles
   renameOps <- liftEither $ renameFiles (episodes season) files
@@ -43,12 +52,26 @@ undoRename (UndoOptions logFileName) = do
   let reversedOps = map undoRenameOp renameOps
   lift $ executeRename reversedOps -- TODO: do I want the undo op to also generate a log?
 
-searchByName :: SearchOptions -> Tvmv ()
-searchByName (SearchOptions maybeApiKey searchQuery) = do
-  apiKey <- liftEither $ populateAPIKey maybeApiKey
+searchByName :: Env -> SearchOptions -> Tvmv ()
+searchByName env (SearchOptions maybeApiKey searchQuery) = do
+  apiKey <- liftEither $ populateAPIKey maybeApiKey env
   showResults <- searchShowByName apiKey searchQuery
   liftIO $ printShows showResults -- TODO: search shouldn't gen a log either...
 
-populateAPIKey :: Maybe APIKey -> Either Error APIKey
-populateAPIKey (Just k) = Right k
-populateAPIKey Nothing = Left $ APIError "Missing API key!" -- TODO: check env and file in this case
+-- We check for API key in the following places, in the following order:
+--
+-- 1) CLI args. If it doesn't exist in CLI args, then check...
+-- 2) The env var. If it doesn't exist in the env var, then check...
+-- 3) The contents of a specific file. If it doesn't exist there, then...
+--
+-- ...return an error! This allows one to easily override the key in a
+-- particular terminal session or even a particular call to `tvmv`. And gives
+-- some flexibility to people who have different security needs/cares.
+populateAPIKey :: Maybe APIKey -> Env -> Either Error APIKey
+populateAPIKey (Just cliArgsKey) _ = Right cliArgsKey -- CLI args take precedence
+populateAPIKey Nothing env = case envOrFile of
+  Just k -> Right k
+  Nothing -> Left missingKeyError
+  where
+    envOrFile = apiKeyEnvVar env <|> apiKeyFile env -- otherwise try these, in this order
+    missingKeyError = InvalidInput "Missing API key! Please provide via command line args, env var, or file"
