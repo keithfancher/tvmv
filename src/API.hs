63a5461583d94a678dddd1aeb9f94440f9174e22
ktf <not.discostoo@gmail.com>
2022-11-19 10:40:12 -0800

    Test out using a type class to abstract away the API calls

--- src/API.hs
+++ src/API.hs
@@ -1,38 +1,52 @@
 module API
   ( APIKey,
+    MonadAPI (..),
     searchSeasonById,
     searchSeasonByName,
     searchShowByName,
   )
 where
 
-import qualified API.TMDB as API -- in theory, we can just swap this import to switch APIs
+import qualified API.TMDB as TMDB
+import Control.Monad.Except (MonadError, liftEither)
 import qualified Data.Text as T
 import Error (Error (..))
 import Show (ItemId, Season (..), TvShow (..))
-import Tvmv (Tvmv, liftEither)
+import Tvmv (Tvmv)
 
 type APIKey = T.Text
 
+-- Testing the API as a type class -- this might not work, as I'll potentially
+-- want to swap APIs out without a whole other Monad stack...
+class Monad m => MonadAPI m where
+  getShow :: APIKey -> ItemId -> m TvShow
+  getSeason :: APIKey -> TvShow -> Int -> m Season
+  queryShows :: APIKey -> T.Text -> m [TvShow]
+
+instance MonadAPI Tvmv where
+  getShow = TMDB.getShow
+  getSeason = TMDB.getSeason
+  queryShows = TMDB.queryShows
+
 -- Search for a show with the given name and, using the FIRST match for that
 -- name, return the episode data for the given season.
-searchSeasonByName :: APIKey -> T.Text -> Int -> Tvmv Season
+searchSeasonByName :: (MonadAPI m, MonadError Error m) => APIKey -> T.Text -> Int -> m Season
 searchSeasonByName apiKey query seasonNum = do
-  showResults <- API.queryShows apiKey query
+  showResults <- queryShows apiKey query
   firstResult <- liftEither $ firstMatch showResults
-  API.getSeason apiKey firstResult seasonNum
+  getSeason apiKey firstResult seasonNum
 
 -- Get season episode data using the show's unique ID directly. Doesn't
 -- actually save on API calls, which is kind of funny, but ensures you're
 -- getting exactly the match you want.
-searchSeasonById :: APIKey -> ItemId -> Int -> Tvmv Season
+searchSeasonById :: MonadAPI m => APIKey -> ItemId -> Int -> m Season
 searchSeasonById apiKey itemId seasonNum = do
-  showData <- API.getShow apiKey itemId
-  API.getSeason apiKey showData seasonNum
+  showData <- getShow apiKey itemId
+  getSeason apiKey showData seasonNum
 
 -- Given a show name, or a fragment of a name, get back a list of matches.
-searchShowByName :: APIKey -> T.Text -> Tvmv [TvShow]
-searchShowByName = API.queryShows
+searchShowByName :: MonadAPI m => APIKey -> T.Text -> m [TvShow]
+searchShowByName = queryShows
 
 firstMatch :: [a] -> Either Error a
 firstMatch [] = Left $ APIError "No match found for search query"
