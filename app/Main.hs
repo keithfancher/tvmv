824c85685fafa0fdbfb14aac1d2d46a319250703
ktf <not.discostoo@gmail.com>
2022-11-30 20:37:40 -0800

    Nicer error message for user abort

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Args (cliOptParser)
+import Domain.Error (Error (..))
 import Env (populateEnv)
 import Execute (execCommand)
 import Options.Applicative (execParser)
@@ -11,5 +12,7 @@ main = do
   env <- populateEnv
   result <- execCommand env command
   case result of
+    -- Specal case for user abort, so it's a bit "nicer":
+    Left UserAbort -> putStrLn "Operation aborted! No files have been renamed."
     Left e -> putStrLn $ "Failed with: " <> show e
     Right _ -> return ()
