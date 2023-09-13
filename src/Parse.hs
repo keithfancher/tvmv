a5533f692a433740cb6440749e3ca9ce4509f6b5
ktf <not.ktf@proton.me>
2023-09-12 21:23:41 -0700

    Parse out season and episode number from full, well-formed filenames

--- src/Parse.hs
+++ src/Parse.hs
@@ -4,7 +4,7 @@ module Parse
   )
 where
 
-import Text.Parsec (ParseError, char, many1, parse, (<|>))
+import Text.Parsec (ParseError, anyChar, char, lookAhead, many, many1, manyTill, parse, try, (<|>))
 import Text.Parsec.Char (digit)
 import Text.Parsec.String (Parser)
 
@@ -21,12 +21,34 @@ data SeasonEpNum = SeasonEpNum
 -- TODO: Actually, map this `ParseError` to an error in our domain. The calling
 -- code shouldn't have to depend on Parsec!
 parseFilename :: FilePath -> Either ParseError SeasonEpNum
-parseFilename = parse parser err
+parseFilename = parse fullFilename err
   where
-    parser = seasonEpNumXFormat <|> seasonEpNumSEFormat
     err = "" -- used only in errors, we don't need it
 
--- e.g. "2x12"
+-- Parses out the season number and episode number, ignoring all leading and
+-- trailing characters.
+fullFilename :: Parser SeasonEpNum
+fullFilename = do
+  _ <- leadingChars
+  seasonEpNumCutTrailing
+  where
+    -- Consume characters until we succeed in parsing ep/season num. We need a
+    -- version of our parser that doesn't *consume* our ep/season number here,
+    -- since `manyTill` throws that bit away.
+    leadingChars = manyTill anyChar epNumLookAhead
+    -- The combination of `try` and `lookAhead` allows checking for a
+    -- successful parse without consuming any input:
+    epNumLookAhead = try $ lookAhead seasonEpNumCutTrailing
+
+-- Consumes (and discards) any trailing characters after successfully parsing
+-- out season and ep numbers.
+seasonEpNumCutTrailing :: Parser SeasonEpNum
+seasonEpNumCutTrailing = do
+  seasonEpNum <- seasonEpNumXFormat <|> seasonEpNumSEFormat
+  _ <- many anyChar
+  return seasonEpNum
+
+-- Consumes, e.g., "2x12"
 seasonEpNumXFormat :: Parser SeasonEpNum
 seasonEpNumXFormat = do
   s <- parseInt
@@ -34,7 +56,7 @@ seasonEpNumXFormat = do
   e <- parseInt
   return SeasonEpNum {seasonNum = s, episodeNum = e}
 
--- e.g. "s02e12"
+-- Consumes, e.g., "s02e12"
 seasonEpNumSEFormat :: Parser SeasonEpNum
 seasonEpNumSEFormat = do
   _ <- char 's' <|> char 'S'
--- test/unit/ParseSpec.hs
+++ test/unit/ParseSpec.hs
@@ -6,14 +6,20 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "parseFilename" $ do
-    it "parses all well-formed filenames" $ do
-      mapM_ testResultSet inputOutputWellFormed
+    it "parses all well-formed base cases (season + ep num ONLY)" $ do
+      mapM_ testResultSet inputOutputBaseCases
+
+    it "parses with trailing characters, ignoring them" $ do
+      mapM_ testResultSet inputOutputTrailing
+
+    it "parses all well-formed full filenames" $ do
+      mapM_ testResultSet inputOutputFullFilename
 
 testResultSet :: (String, SeasonEpNum) -> Expectation
 testResultSet (input, expected) = parseFilename input `shouldBe` Right expected
 
-inputOutputWellFormed :: [(String, SeasonEpNum)]
-inputOutputWellFormed =
+inputOutputBaseCases :: [(String, SeasonEpNum)]
+inputOutputBaseCases =
   [ ("1x21", SeasonEpNum {seasonNum = 1, episodeNum = 21}),
     ("001x021", SeasonEpNum {seasonNum = 1, episodeNum = 21}), -- zero-padded
     ("1X21", SeasonEpNum {seasonNum = 1, episodeNum = 21}), -- uppercase
@@ -21,4 +27,18 @@ inputOutputWellFormed =
     ("s001e021", SeasonEpNum {seasonNum = 1, episodeNum = 21}), -- more zeros
     ("S01E21", SeasonEpNum {seasonNum = 1, episodeNum = 21}), -- uppercase
     ("S01e21", SeasonEpNum {seasonNum = 1, episodeNum = 21}) -- mixed case
+  ]
+
+inputOutputTrailing :: [(String, SeasonEpNum)]
+inputOutputTrailing =
+  [ ("1x21anythingelseafterthis", SeasonEpNum {seasonNum = 1, episodeNum = 21}),
+    ("s01e21 - an ep or something", SeasonEpNum {seasonNum = 1, episodeNum = 21}),
+    ("s001e021 and then a contradiction s03e05", SeasonEpNum {seasonNum = 1, episodeNum = 21})
+  ]
+
+inputOutputFullFilename :: [(String, SeasonEpNum)]
+inputOutputFullFilename =
+  [ ("A pretty cool TV show - 1x21 - An alright episode", SeasonEpNum {seasonNum = 1, episodeNum = 21}),
+    ("Buffy - s09e99 - Buffy's wedding", SeasonEpNum {seasonNum = 9, episodeNum = 99}),
+    ("Poirot - s035e123 - A confusing s12e04 episode title", SeasonEpNum {seasonNum = 35, episodeNum = 123})
   ]
