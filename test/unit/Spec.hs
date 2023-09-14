73e78ae9b918537f01ad2a2c99edea46bd305e75
ktf <not.ktf@proton.me>
2023-09-13 19:29:39 -0700

    Use `hspec-discover` to remove annoying test boilerplate
    BUT to this without getting stupid warnings, also had to upgrade `hspec`
    and its associated packages. The bug was fixed in an `hspec` version
    newer than what's in our Stackage LTS.

--- test/unit/Spec.hs
+++ test/unit/Spec.hs
@@ -1,19 +1,2 @@
-import API.TMDBSpec qualified
-import Domain.RenameSpec qualified
-import Exec.EnvSpec qualified
-import FileSpec qualified
-import LogSpec qualified
-import ParseSpec qualified
-import Test.Hspec
-
-main :: IO ()
-main = hspec spec
-
-spec :: Spec
-spec = do
-  describe "API.TMDB" API.TMDBSpec.spec
-  describe "Domain.Rename" Domain.RenameSpec.spec
-  describe "Exec.Env" Exec.EnvSpec.spec
-  describe "File" FileSpec.spec
-  describe "Log" LogSpec.spec
-  describe "Parse" ParseSpec.spec
+-- spec discovery magic: https://hspec.github.io/hspec-discover.html
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
