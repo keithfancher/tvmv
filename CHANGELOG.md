2212d7cb990b3c918e66e777d0f88d83b9f8031a
ktf <not.ktf@proton.me>
2024-01-11 16:01:03 -0800

    Version `0.3.0`!

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,7 @@ and this project adheres to the
 [Haskell Package Versioning Policy](https://pvp.haskell.org/).
 
 
-## Unreleased
+## 0.3.0 - 2024-01-11
 
 ### Added
 
--- app/Version.hs
+++ app/Version.hs
@@ -2,4 +2,4 @@ module Version (currentVersion) where
 
 -- Just updating this manually for now...
 currentVersion :: String
-currentVersion = "0.3.0-dev"
+currentVersion = "0.3.0"
