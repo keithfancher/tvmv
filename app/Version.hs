df3babc51f51a20c30f54b1be2a5f19abbe35372
ktf <not.ktf@proton.me>
2024-01-19 14:04:50 -0800

    Bump version string to new dev version

--- app/Version.hs
+++ app/Version.hs
@@ -1,5 +1,6 @@
 module Version (currentVersion) where
 
--- Just updating this manually for now...
+-- Just updating this manually for now. Note that changing this string doesn't
+-- actually *do* anything -- it's just what's shown to the user in `--help`.
 currentVersion :: String
-currentVersion = "0.3.0"
+currentVersion = "0.4.0-dev"
