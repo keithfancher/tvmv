a9a07dcfa0516445ab243e7f89baa42022109617
ktf <not.ktf@proton.me>
2024-01-26 16:09:10 -0800

    Move `undo` into its own module, further breaking apart `Exec.Commands`

--- src/Exec/Commands.hs
+++ src/Exec/Commands.hs
@@ -1,6 +1,5 @@
 module Exec.Commands
-  ( undoRename,
-    searchByName,
+  ( searchByName,
   )
 where
 
@@ -11,32 +10,14 @@ where
 -- are not coupled to any particular monad or monad transformer stack.)
 
 import API (searchShowByName)
-import Command (SearchOptions (..), UndoOptions (..))
+import Command (SearchOptions (..))
 import Control.Monad.Except (MonadError, liftEither)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Writer (MonadWriter)
 import Domain.API (APIWrapper)
 import Domain.Error (Error (..))
-import Domain.Rename (RenameOp, undoRenameOp)
 import Exec.Env (Env, populateAPIKey)
-import Exec.Rename (RenameResult, executeRename, makeOpRelative)
-import Log (readLatestLogFile, readLogFile)
 import Print (prettyPrintListLn)
 import Text.Printf (printf)
-
--- Undo a previously-run rename operation, given a log file. The `undo` command.
-undoRename ::
-  (MonadIO m, MonadError Error m, MonadWriter [RenameResult] m) =>
-  UndoOptions ->
-  m ()
-undoRename (UndoOptions forceRename maybeLogFileName) = do
-  renameOps <- readLog maybeLogFileName
-  let reversedOps = map undoRenameOp renameOps
-  runRenameOps reversedOps (undoMsg reversedOps) forceRename
-  where
-    undoMsg f = printf "Undoing will result in the following %d rename operations...\n" (length f)
-    readLog (Just fileName) = readLogFile fileName
-    readLog Nothing = readLatestLogFile
 
 -- Query the configured API for a show with the given name. The `search` command!
 searchByName ::
---
+++ src/Exec/Undo.hs
@@ -0,0 +1,25 @@
+module Exec.Undo (undoRename) where
+
+import Command (UndoOptions (..))
+import Control.Monad.Except (MonadError)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Writer (MonadWriter)
+import Domain.Error (Error (..))
+import Domain.Rename (undoRenameOp)
+import Exec.Rename (RenameResult, runRenameOps)
+import Log (readLatestLogFile, readLogFile)
+import Text.Printf (printf)
+
+-- Undo a previously-run rename operation, given a log file. The `undo` command.
+undoRename ::
+  (MonadIO m, MonadError Error m, MonadWriter [RenameResult] m) =>
+  UndoOptions ->
+  m ()
+undoRename (UndoOptions forceRename maybeLogFileName) = do
+  renameOps <- readLog maybeLogFileName
+  let reversedOps = map undoRenameOp renameOps
+  runRenameOps reversedOps (undoMsg reversedOps) forceRename
+  where
+    undoMsg f = printf "Undoing will result in the following %d rename operations...\n" (length f)
+    readLog (Just fileName) = readLogFile fileName
+    readLog Nothing = readLatestLogFile
