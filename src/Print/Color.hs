55801ad7ca75c0c6d7e6025b21e34b71265c7d11
ktf <not.ktf@proton.me>
2024-02-20 13:06:00 -0800

    Simplify color-printing code a bit
    Also make sure the type class functions are defined in terms of *other*
    type class functions, so users can get away with overriding as little as
    possible.

--- src/Print/Color.hs
+++ src/Print/Color.hs
@@ -8,6 +8,7 @@ module Print.Color
 where
 
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.List (intersperse)
 import Data.Text (Text)
 import Data.Text.IO qualified as TIO
 import System.Console.ANSI (Color (..), ColorIntensity (..), ConsoleLayer (..), SGR (..), setSGR)
@@ -30,15 +31,20 @@ class Colorized a where
   printColorized :: (MonadIO m) => a -> m ()
   printColorizedLn :: (MonadIO m) => a -> m ()
   printColorizedList :: (MonadIO m) => [a] -> m ()
+  printColorizedListLn :: (MonadIO m) => [a] -> m ()
 
   -- Default implementations:
   printColorized = printColor . colorize
-  printColorizedLn = printColorLn . colorize
-  printColorizedList = mapM_ withseps
+  printColorizedLn = withNewline . printColorized
+
+  printColorizedList l = liftIO iosWithSep
     where
-      withseps x = do
-        printColorized x
-        liftIO $ putStr "\n\n"
+      colorizedIOs = map printColorized l
+      printSep = putStr "\n\n"
+      -- Blows my mind that you can alter a list of IO actions like this!
+      iosWithSep = sequence_ $ intersperse printSep colorizedIOs
+
+  printColorizedListLn = withNewline . printColorizedList
 
 printColor :: (MonadIO m) => ColorText -> m ()
 printColor (R t) = withSGR (fgColor Red) $ TIO.putStr t
@@ -48,7 +54,7 @@ printColor (N t) = liftIO $ TIO.putStr t
 printColor (Arr arr) = mapM_ printColor arr
 
 printColorLn :: (MonadIO m) => ColorText -> m ()
-printColorLn c = liftIO $ printColor c >> putStrLn ""
+printColorLn = withNewline . printColor
 
 printError :: (MonadIO m) => Text -> m ()
 printError msg = printColorLn $ R msg
@@ -68,3 +74,7 @@ withSGR sgr act = liftIO $ do
   setSGR sgr
   act
   setSGR [Reset]
+
+-- Do an IO action, then print a "\n" afterward.
+withNewline :: (MonadIO m) => m () -> m ()
+withNewline act = act >> liftIO (putStrLn "")
