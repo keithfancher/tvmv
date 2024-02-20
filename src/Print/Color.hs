81415ff5a7f92f36155e9a8dffd0be0781ddd456
ktf <not.ktf@proton.me>
2024-02-19 21:14:41 -0800

    Create `ColorText` data type to make dealing with colored output easier

--- src/Print/Color.hs
+++ src/Print/Color.hs
@@ -8,14 +8,40 @@ where
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import System.Console.ANSI (Color (..), ColorIntensity (..), ConsoleLayer (..), SGR (..), setSGR)
 
+data ColorText
+  = R String -- red
+  | G String -- green
+  | Y String -- yellow
+  | N String -- no color, default
+  | Arr [ColorText]
+
+instance Semigroup ColorText where
+  (<>) (Arr a) (Arr b) = Arr (a ++ b)
+  (<>) (Arr a) c = Arr (a ++ [c])
+  (<>) c (Arr a) = Arr (c : a)
+  (<>) c1 c2 = Arr (c1 : [c2])
+
+instance Monoid ColorText where
+  mempty = Arr []
+
+printColor :: (MonadIO m) => ColorText -> m ()
+printColor (R s) = withSGR (fgColor Red) $ putStr s
+printColor (Y s) = withSGR (fgColor Yellow) $ putStr s
+printColor (G s) = withSGR (fgColor Green) $ putStr s
+printColor (N s) = liftIO $ putStr s
+printColor (Arr arr) = mapM_ printColor arr
+
+printColorLn :: (MonadIO m) => ColorText -> m ()
+printColorLn c = liftIO $ printColor c >> putStrLn ""
+
 printError :: (MonadIO m) => String -> m ()
-printError msg = withSGR (fgColor Red) $ putStrLn msg
+printError msg = printColorLn $ R msg
 
 printSuccess :: (MonadIO m) => String -> m ()
-printSuccess msg = withSGR (fgColor Green) $ putStrLn msg
+printSuccess msg = printColorLn $ G msg
 
 printWarning :: (MonadIO m) => String -> m ()
-printWarning msg = withSGR (fgColor Yellow) $ putStrLn msg
+printWarning msg = printColorLn $ Y msg
 
 fgColor :: Color -> [SGR]
 fgColor c = [SetColor Foreground Dull c]
