2773e30c4556d6b3bc21c9b86b72a7744589dc63
ktf <not.ktf@proton.me>
2024-02-24 13:46:09 -0800

    Colorize log file names as well!

--- src/Log.hs
+++ src/Log.hs
@@ -15,14 +15,14 @@ import Control.Monad.Except (MonadError, liftEither, throwError)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.List (isPrefixOf, sortBy)
 import Data.Maybe (listToMaybe)
-import Data.Text qualified as T
+import Data.Text (Text, pack, unpack)
 import Data.Text.IO qualified as TIO
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import Domain.Error (Error (..))
 import Domain.Rename (RenameOp)
 import Exec.Rename (RenameResult (..), getOp, makeResultRelative)
 import Monad.Tvmv (Logger)
-import Print.Color (Colorized (..), printError, printSuccess, printWarning)
+import Print.Color (ColorText, Colorized (..), cyan, printColorLn, printError, printSuccess, printWarning)
 import System.Directory (listDirectory)
 import System.FilePath (takeFileName)
 import Text.Printf (printf)
@@ -36,8 +36,14 @@ writeLogFile :: Logger
 writeLogFile [] = return () -- no ops, don't log
 writeLogFile results = do
   logFile <- logFileName
-  putStrLn $ "Writing log to `" <> logFile <> "`.\nUse `tvmv undo` to undo any changes made by this operation."
+  printColorLn $
+    "Writing log to "
+      <> colorLog logFile
+      <> ".\nUse `tvmv undo` to undo any changes made by this operation."
   TIO.writeFile logFile (getLogText results)
+
+colorLog :: FilePath -> ColorText
+colorLog = cyan . pack
 
 -- Print only, don't write a log file.
 printLog :: Logger
@@ -50,7 +56,7 @@ printLog results = do
     totalOps = length results
     numSuccesses = length $ successfulOps results
     numFailures = totalOps - numSuccesses
-    finalCountMsg = T.pack $ show numSuccesses <> "/" <> show totalOps <> " operations succeeded. " <> show numFailures <> " failures.\n"
+    finalCountMsg = pack $ show numSuccesses <> "/" <> show totalOps <> " operations succeeded. " <> show numFailures <> " failures.\n"
     printResultsColor = case (numSuccesses, totalOps) of
       (suc, tot) | suc == tot -> printSuccess
       (0, _) -> printError
@@ -61,12 +67,12 @@ printAndWriteLog :: Logger
 printAndWriteLog results = printLog results >> writeLogFile results
 
 -- Given a set of results, get the text to be written to a log file.
-getLogText :: [RenameResult] -> T.Text
+getLogText :: [RenameResult] -> Text
 getLogText results = textify successes
   where
     -- Logfile is just `show`-ing the list of operations, we'll `read` it on
     -- the other end for `undo`:
-    textify = T.pack . show
+    textify = pack . show
     successes = successfulOps results
 
 successfulOps :: [RenameResult] -> [RenameOp]
@@ -102,12 +108,12 @@ isLogFile f = tvmvLogBaseFilename `isPrefixOf` fileWithoutPath
 -- out the logged operations.
 readLogFile :: (MonadIO m, MonadError Error m) => FilePath -> m [RenameOp]
 readLogFile logFile = do
-  liftIO $ putStrLn $ "Reading previously-completed operations from log file: " <> logFile
+  printColorLn $ "Reading previously-completed operations from log file: " <> colorLog logFile
   text <- liftIO $ TIO.readFile logFile
   liftEither $ renameOpsFromText text
 
-renameOpsFromText :: T.Text -> Either Error [RenameOp]
-renameOpsFromText t = case readMaybe (T.unpack t) of
+renameOpsFromText :: Text -> Either Error [RenameOp]
+renameOpsFromText t = case readMaybe (unpack t) of
   Just results -> Right results
   Nothing -> Left $ UndoError "Invalid log file, unable to parse"
 
