04f4a1770c334d3cd1c9eb50e8a27144108e5dd2
ktf <not.ktf@proton.me>
2024-02-29 13:48:37 -0800

    Version `0.5.0`!

--- app/Version.hs
+++ app/Version.hs
@@ -3,4 +3,4 @@ module Version (currentVersion) where
 -- Just updating this manually for now. Note that changing this string doesn't
 -- actually *do* anything -- it's just what's shown to the user in `--help`.
 currentVersion :: String
-currentVersion = "0.5.0-dev"
+currentVersion = "0.5.0"
